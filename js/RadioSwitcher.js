b19218f87db783cd9f2dab9272817f66a4f1fa7a
Ryan Morlok <ryanmorlok@Fozzy.local>
2010-08-24 23:25:08 -0500

    sam's changes

---
+++ js/RadioSwitcher.js
@@ -0,0 +1,36 @@
+﻿(function() {
+	window.RadioSwitcher = function(parent, pairs, name) {
+		var me = this;
+		me.container = $(document.createElement('div')).appendTo($(parent));
+		me.blocks = $(document.createElement('div')).appendTo($(parent));
+		me.buttonList = $(document.createElement('ul')).appendTo(me.container);
+		
+		$.each(pairs, function(label, id) {
+			var listItem = $(document.createElement('li')).appendTo(me.buttonList);
+			
+			$(document.createElement('input'))
+				.attr('type', 'radio')
+				.attr('class', 'switcher')
+				.attr('id', 'RadioButtonFor_' + id)
+				.attr('target', id)
+				.attr('name', name)
+				.appendTo(listItem);
+
+			$(document.createElement('label'))
+				.attr('for', 'RadioButtonFor_' + id)
+				.html(label)
+				.appendTo(listItem);
+
+			$('#' + id)
+				.appendTo(me.blocks)
+				.addClass('switchee')
+				.hide();
+		});
+
+		me.buttonList.find('.switcher').click(function() {
+			var id = $(this).attr('target');
+			$('.switchee:visible[id!=' + id + ']').slideUp();
+			$('#' + id).slideDown();
+		});
+	}
+})();
